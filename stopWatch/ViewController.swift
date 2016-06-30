e41eff45f053dfae130ce66738f816bb0e9e76d1
tuğçe <tugceulutass@gmail.com>
2016-06-30 20:23:41 +0300

    Initial Commit

---
+++ stopWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stopWatch
+//
+//  Created by tuğçe on 30/06/16.
+//  Copyright © 2016 tuğçe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ stopWatchTests/stopWatchTests.swift
@@ -0,0 +1,36 @@
+//
+//  stopWatchTests.swift
+//  stopWatchTests
+//
+//  Created by tuğçe on 30/06/16.
+//  Copyright © 2016 tuğçe. All rights reserved.
+//
+
+import XCTest
+@testable import stopWatch
+
+class stopWatchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
