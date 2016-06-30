1a8c5f9cdc6e09d29a7c7bdf95c7a5d1eb77a318
tuğçe <tugceulutass@gmail.com>
2016-06-30 22:41:56 +0300

    stopWatch created

--- stopWatch/ViewController.swift
+++ stopWatch/ViewController.swift
@@ -9,16 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var timer=NSTimer()
+    var time=0
+    func increaseTimer(){
+        time = time + 1
+        timerLabel.text="\(time)"
+        
+    }
 
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBAction func play(sender: AnyObject) {
+        
+        timer=NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: (#selector(ViewController.increaseTimer)), userInfo: nil, repeats: true)
+        
+            }
+    @IBAction func pause(sender: AnyObject) {
+        
+        timer.invalidate()
+    }
+    @IBAction func reset(sender: AnyObject) {
+        timer.invalidate()
+        time=0
+        timerLabel.text="0"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+ 
 
 
 }
